85910aedc56c2fc92ea61bd7dd163ec5647678b2
ishay-shaer <158286872+ishay-shaer@users.noreply.github.com>
2024-03-12 17:01:23 +0200

    Update index.js
    Adding the "\r" split because I have no idea how to change the text file

--- index.js
+++ index.js
@@ -552,7 +552,7 @@ async function getTextfromFile(filePath) {
 
 async function getWordsFromTextFile(filePath) {
     const text = await getTextfromFile(filePath);
-    return text.split("\n");
+    return text.split("\n").split("\r");
 }
 
 async function getPossibleGuesses(wordLength, magicWords) {
