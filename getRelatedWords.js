a440cc67ec585db576e9717987571969a91e8972
ishay-shaer <158286872+ishay-shaer@users.noreply.github.com>
2024-03-12 16:15:15 +0200

    Update getRelatedWords.js
    Updating to a newer version

--- getRelatedWords.js
+++ getRelatedWords.js
@@ -1,18 +1,27 @@
 "use strict";
 
-const dataFilePath = "/data_5_letters.json";
+const DEFAULT_WORD_LENGTH = 5;
 
-export default async function getRandomRelatedWords() {
+export async function getRandomRelatedWords(wordLength=DEFAULT_WORD_LENGTH, exceptions=null) {
+    const wordsObject = await getWordsObject(wordLength, exceptions);
+    const randomWord = getRandomElFromArray(Object.keys(wordsObject));
+    const relatedWord = getRandomElFromArray(wordsObject[randomWord]);
+    return [randomWord, relatedWord];
+}
+
+export async function getRandomWord(wordLength=DEFAULT_WORD_LENGTH, exceptions=null) {
+    return getRandomElFromArray(Object.keys(await getWordsObject(wordLength, exceptions)));
+}
+
+async function getWordsObject(wordLength, exceptions=null) {
+    const dataFilePath = `./data_${wordLength}_letters.json`;
+    if (!exceptions) exceptions = [];
     const response = await fetch(dataFilePath);
     if (!response.ok) {
         throw new Error("Network response was not ok");
     }
     const data = await response.text();
-    const dataObject = JSON.parse(data);
-    const primaryWords = Object.keys(dataObject);
-    const randomWord = getRandomElFromArray(primaryWords);
-    const relatedWord = getRandomElFromArray(dataObject[randomWord]);
-    return [randomWord, relatedWord];
+    return JSON.parse(data);
 }
 
 function getRandomElFromArray(arr) {
