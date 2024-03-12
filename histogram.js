c045209b62264e6d99ed5a20a9a0d8ff3e4d1bf3
ishay-shaer <158286872+ishay-shaer@users.noreply.github.com>
2024-03-12 16:13:05 +0200

    Update histogram.js
    Updating to use DOM manipulation methods and the createElement module

--- histogram.js
+++ histogram.js
@@ -1,42 +1,37 @@
 "use strict";
 
+import { el } from "./createElement.js";
 const GAP_WIDTH_RATIO = .1;
 const PERCENT_DIGITS = 0;
 
-export default function createHistogram(divEl, dataObject, xLabel="x", highlightedBar=null, keys=null) {
+export default function createHistogram(dataObject, xLabel="x", highlightedBar=null, keys=null) {    
     if (!keys) keys = Object.keys(dataObject).sort((a, b) => a - b);
-    const values = Object.values(dataObject);
-    const chartContainer = document.createElement("div");
-    chartContainer.id = "chart-container";
+    const values = Object.values(dataObject);    
     const maxValue = Math.max(...values);
     const sumValues = values.reduce((total, value) => total + value, 0);
-    
     const stringData = {};
     keys.forEach(key => {
         const numericValue = dataObject[key];
         const percentValue = (numericValue / sumValues * 100).toFixed(PERCENT_DIGITS);
         stringData[key] = numericValue ? `${numericValue}<br>(${percentValue}%)` : "";
     });
-
-    let chartContent = "";
-
+    const chartContainer = el("div", {class: "chart-container"});
     for (const key of keys) {
-        const height = (dataObject[key] / maxValue * .75 * divEl.style.height.replace("px", "")) + "px";
-        chartContent += `<span class="data-column" id="data-column-${key}">
-                             <div class="datum">${stringData[key]}</div>
-                             <div class="bar" id="key-${key}-bar" style="height: ${height};"></div>
-                             <div class="num-guesses">${key}</div>
-                         </span>`;
+        const height = (dataObject[key] / maxValue * 75) + "%";
+        chartContainer.appendChild(el("div", {
+            id: `data-column-${key}`,
+            classList: ["data-column", key === highlightedBar ? "highlighted-column" : null],
+            children: [
+                el("div", {class: "num-guesses", textContent: key}),
+                el("div", {class: "bar", id: `key-${key}-bar`, style: {height: height}}),
+                el("div", {class: "datum", innerHTML: stringData[key]})
+            ]
+        }))
     }
-    
-    chartContainer.innerHTML = chartContent;
-    divEl.appendChild(chartContainer);
-    if (highlightedBar != null)
-        document.querySelector(`#data-column-${highlightedBar}`).classList.add("highlighted-column");
-
-    chartContainer.style.gridTemplateColumns = `repeat(${keys.length}, auto)`;
     const gapWidth = ((100 / (keys.length - 1)) * GAP_WIDTH_RATIO) + "%";
     chartContainer.style.columnGap = gapWidth;
-    
-    divEl.innerHTML += `<div id='x-label'>${xLabel}</div>`;
+    return el("div", {
+        class: "chart-box",
+        children: [chartContainer, el("div", {id:"x-label", textContent: xLabel})]
+    });
 }
